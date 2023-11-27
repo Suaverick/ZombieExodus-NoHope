6c394fd2ee74debcab2446981fc04d30e109a3c5
Suaverick <James.norville@outlook.com>
2023-11-26 23:50:09 -0500

    Second commit
    This includes player and AI animations and models, a rifle model, character and AI damage and health

---
+++ Source/ZombieExodus/ZombieExodusGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ZombieExodusGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AZombieExodusGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AZombieExodusGameMode();
+};
+
+
+
